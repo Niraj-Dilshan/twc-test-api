b57d64e2d9f9a2b6bac4b8a0ab88acf57ad4013a
niraj <nirajnimantha@gmail.com>
2024-04-13 21:28:20 +0530

    Add email validation function in validation.js

---
+++ src/utils/validation.js
@@ -0,0 +1,8 @@
+function validateEmail(email) {
+    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return emailRegex.test(email);
+}
+
+module.exports = {
+    validateEmail
+};
